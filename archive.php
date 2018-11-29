09c7e9f3e2f62b8d93f2d23a43af0f11108c90cd
北门清燕 <qq88268459@126.com>
2018-11-29 13:43:42 +0800

    update

--- archive.php
+++ archive.php
@@ -5,31 +5,33 @@
 
 get_header(); ?>
 
-<div class="main-inner">
-    <div class="content-wrap">
-        <div id="content" class="content">
-            <section id="posts" class="posts-collapse">
-	            <?php
-	            // Start the loop.
-	            while ( have_posts() ) : the_post();
+    <div class="main-inner">
+        <div class="content-wrap">
+            <div id="content" class="content">
+                <div class="post-block">
+                    <section id="posts" class="posts-collapse">
+                        <?php
+                        // Start the loop.
+                        while ( have_posts() ) : the_post();
 
-		            bmqynext_archives_list();
+                            bmqynext_archives_list();
 
-		            // If comments are open or we have at least one comment, load up the comment template.
-		            if ( comments_open() || get_comments_number() ) {
-			            comments_template();
-		            }
+                            // If comments are open or we have at least one comment, load up the comment template.
+                            if ( comments_open() || get_comments_number() ) {
+                                comments_template();
+                            }
 
-		            // End of the loop.
-	            endwhile;
-	            ?>
-            </section>
+                            // End of the loop.
+                        endwhile;
+                        ?>
+                    </section>
+                </div>
+            </div>
+            <?php get_sidebar( 'content-bottom' ); ?>
         </div>
-	    <?php get_sidebar( 'content-bottom' ); ?>
-    </div>
 
 
-</div><!-- .content-area -->
-
+    </div><!-- .content-area -->
+</main>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
